65ae5d9d81a4c2bc7c2b96f195811330fe3e7107
Apriadi <52624313+apriadi4425@users.noreply.github.com>
2020-06-11 11:01:15 +0700

    Perbaikan konfig.php
    Memperbaiki konfig.php

--- konfig.php
+++ konfig.php
@@ -2,12 +2,16 @@
 
 
 class Database {
-	public $host_db,$user_db, $pass_db,$db1,$db2;
+	public $host_db1,$user_db1, $pass_db1,$host_db2,$user_db2, $pass_db2,$db1,$db2;
 
-	public function get_database($host_db, $user_db, $pass_db, $db1, $db2){
-		$this->host_db = $host_db;
-		$this->user_db = $user_db;
-		$this->pass_db = $pass_db;
+	public function get_database($host_db1, $user_db1, $pass_db1,$host_db2, $user_db2, $pass_db2, $db1, $db2){
+		$this->host_db1 = $host_db1;
+		$this->user_db1 = $user_db1;
+		$this->pass_db1 = $pass_db1;
+		
+		$this->host_db2 = $host_db2;
+		$this->user_db2 = $user_db2;
+		$this->pass_db2 = $pass_db2;
 		$this->db1 = $db1;
 		$this->db2 = $db2;
 	}
